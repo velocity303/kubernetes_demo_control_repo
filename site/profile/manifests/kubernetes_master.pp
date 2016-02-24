10e710db12ef3517d97566fcda6ad77b08683220
James E. Jones <james.jones@puppetlabs.com>
2016-02-23 19:53:19 -0600

    add quotes around parameters

--- site/profile/manifests/kubernetes_master.pp
+++ site/profile/manifests/kubernetes_master.pp
@@ -15,21 +15,21 @@ class profile::kubernetes_master {
     ensure  => present,
     path    => '/etc/etcd/etcd.conf',
     setting => 'ETCD_DATA_DIR',
-    value   => '/var/lib/etcd/default.etcd',
+    value   => '"/var/lib/etcd/default.etcd"',
   }
 
   ini_setting { 'etcd_listen_client_urls setting':
     ensure  => present,
     path    => '/etc/etcd/etcd.conf',
     setting => 'ETCD_LISTEN_CLIENT_URLS',
-    value   => 'http://0.0.0.0:2379',
+    value   => '"http://0.0.0.0:2379"',
   }
 
   ini_setting { 'etcd_advertise_client_urls setting':
     ensure  => present,
     path    => '/etc/etcd/etcd.conf',
     setting => 'ETCD_ADVERTISE_CLIENT_URLS',
-    value   => 'http://localhost:2379',
+    value   => '"http://localhost:2379"',
   }
 
 }
