cae13bf4869c0de761478bb5423a00d62ac1228c
James E. Jones <james.jones@puppetlabs.com>
2016-02-24 02:48:50 -0600

    update to include kubernetes test

---
+++ site/profile/manifests/test.pp
@@ -0,0 +1,17 @@
+class profile::test {
+
+
+  kubernetes_pod { 'sample-pod':
+    ensure      => present,
+    metadata    => {
+      namespace => 'default',
+    },
+    spec         => {
+      containers => [{
+        name     => 'container-name',
+        image    => 'nginx',
+        }]
+    },
+  }
+
+}
